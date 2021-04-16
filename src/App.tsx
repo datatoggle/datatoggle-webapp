eacd94373bd7fd94f369396458ed22db7e667b60
Nicolas <nicolas.lecrique+git@globers.co>
2021-04-16 08:49:29 +0200

    WIP first version of authentication
    * firebase dependencies are know loaded from npm instead of assuming
    we are in firebase hosting env (to test it locally)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import {StyledFirebaseAuth} from 'react-firebaseui'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD33FHNwomuZ43VUBgtOW4dJ3ePUIRAcps",
+  authDomain: "datatoggle-b83b6.firebaseapp.com",
+  projectId: "datatoggle-b83b6",
+  storageBucket: "datatoggle-b83b6.appspot.com",
+  messagingSenderId: "771767793475",
+  appId: "1:771767793475:web:2300917ead156fc80921f6",
+  measurementId: "G-EHS9SKZVVH"
+};
+
+// Configure FirebaseUI.
+const authUiConfig = {
+  // Popup signin flow rather than redirect flow.
+  //signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [{
+    provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    requireDisplayName: false,
+  }
+  ]
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledFirebaseAuth uiConfig={authUiConfig} firebaseAuth={firebase.auth()} />
     </div>
   );
 }
