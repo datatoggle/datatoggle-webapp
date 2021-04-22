3054010713148f37a96a4c9ab8c2546593b915af
Nicolas <nicolas.lecrique+git@globers.co>
2021-04-22 14:55:27 +0200

    Add proper logout on app bat

--- src/components/MyAppBar.tsx
+++ src/components/MyAppBar.tsx
@@ -1,10 +1,12 @@
-import React, {FunctionComponent, useState} from 'react'
-import {AppBar, IconButton, Link, Toolbar} from '@material-ui/core'
+import React, {FunctionComponent, useContext, useState} from 'react'
+import {AppBar, IconButton, Link, Menu, MenuItem, Toolbar} from '@material-ui/core'
 import logo from '../images/logo.png'
 import {makeStyles} from '@material-ui/core/styles'
 import {AccountCircle} from '@material-ui/icons'
 import {HOME_URL} from '../service/urls'
-import {Redirect} from 'react-router-dom'
+import {UserContext} from '../service/UserContext'
+import {userContext} from './AuthCheck'
+import firebase from 'firebase/app'
 
 
 const useStyles = makeStyles({
@@ -25,6 +27,24 @@ type Props = OwnProps;
 const MyAppBar: FunctionComponent<Props> = (props) => {
   const classes = useStyles();
 
+  const ctx = useContext<UserContext>(userContext)
+
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const logout = async () => {
+    setAnchorEl(null);
+    await firebase.auth().signOut()
+  };
+
   return (<AppBar position="static" style={{background: 'transparent', boxShadow: 'none'}}>
     <Toolbar>
       <Link href={HOME_URL}>
@@ -32,14 +52,25 @@ const MyAppBar: FunctionComponent<Props> = (props) => {
       </Link>
       <div className={classes.grow} />
       <IconButton
+        aria-controls="simple-menu"
         edge="end"
         aria-label="account of current user"
         aria-haspopup="true"
-        onClick={()=> null}
+        onClick={handleClick}
       >
         <AccountCircle />
       </IconButton>
-      
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={anchorEl !== null}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={() => null}>{ctx.fullName}</MenuItem>
+        <MenuItem onClick={logout}>Logout</MenuItem>
+      </Menu>
+
     </Toolbar></AppBar>);
 };
 
