2d49bc25c399292f80f02ec4436598625912b3a0
Nicolas <nicolas.lecrique+git@globers.co>
2021-04-22 16:12:07 +0200

    finish project list page style

--- src/pages/ProjectListPage.tsx
+++ src/pages/ProjectListPage.tsx
@@ -1,45 +1,102 @@
 import React, {FunctionComponent, useContext, useEffect, useState} from 'react'
-import { Redirect } from 'react-router-dom';
+import {Redirect} from 'react-router-dom'
 import {userContext} from '../components/AuthCheck'
 import {ProjectSnippet} from '../service/restapi/data'
-import {NEW_PROJECT_URL} from '../service/urls'
+import { NEW_PROJECT_URL, projectUrl} from '../service/urls'
 import {UserContext} from '../service/UserContext'
 import {makeStyles} from '@material-ui/core/styles'
 import MyAppBar from '../components/MyAppBar'
+import {Card, Link} from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
+import AddIcon from '@material-ui/icons/Add'
+import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles(theme => ({
   offset: theme.mixins.toolbar,
+  centerColumnContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%'
+  },
+  cardContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  projectCard: {
+    width: '292px',
+    height: '196px',
+    borderRadius: '16px',
+    margin: '16px'
+  },
+  button: {
+    height: '100%',
+    width: '100%',
+    textTransform: 'none',
+  }
 }))
 
 
-interface OwnProps {}
+interface OwnProps {
+}
 
 type Props = OwnProps;
+
 
 const ProjectListPage: FunctionComponent<Props> = (props) => {
 
   const ctx: UserContext = useContext(userContext)
-  const [data, setData] = useState<ProjectSnippet[] | null>(null);
+  const [projects, setProjects] = useState<ProjectSnippet[] | null>(null)
 
   useEffect(() => {
-    ctx.api.getProjectSnippets().then((result: ProjectSnippet[]) => {setData(result)})
+    ctx.api.getProjectSnippets().then((result: ProjectSnippet[]) => {
+      setProjects(result)
+    })
   }, [ctx])
 
-  const classes = useStyles();
+  const classes = useStyles()
 
-  if (data) {
-    if (data.length === 0){
-     return <Redirect to={NEW_PROJECT_URL} />
+  if (projects) {
+    if (projects.length === 0) {
+      return <Redirect to={NEW_PROJECT_URL}/>
     } else {
       return <>
         <MyAppBar/>
-        <h1>PROJECT LIST PAGE</h1>
-        <h1>{ctx.email}</h1>
+        <div className={classes.cardContainer}>
+          <Card className={classes.projectCard} onClick={() => null}>
+            <Link href={NEW_PROJECT_URL} underline={'none'}>
+            <Button className={classes.button}>
+              <div className={classes.centerColumnContainer}>
+                <AddIcon color={'primary'}/>
+                <Typography variant="h6" component="h2" color={'primary'}>
+                  Create new project
+                </Typography>
+              </div>
+            </Button>
+            </Link>
+          </Card>
+          <>
+            {projects.map((p: ProjectSnippet) => (
+              <Card className={classes.projectCard} onClick={() => null}>
+                <Link href={projectUrl(p.uri)} underline={'none'}>
+                  <Button className={classes.button}>
+                    <div className={classes.centerColumnContainer}>
+                      <Typography variant="h6" component="h2">
+                        {p.name}
+                      </Typography>
+                    </div>
+                  </Button>
+                </Link>
+              </Card>))}
+          </>
+        </div>
       </>
     }
   } else {
     return <h1>LOADING PROJECTS</h1>
   }
-};
+}
 
-export default ProjectListPage;
+export default ProjectListPage
