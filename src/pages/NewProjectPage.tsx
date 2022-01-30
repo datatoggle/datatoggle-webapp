e1461b391ca01886fc393cd1538c1fe762b9ada1
Nicolas <nicolas.lecrique+git@globers.co>
2022-01-30 22:13:39 +0100

    Rework new project page design

--- src/pages/NewProjectPage.tsx
+++ src/pages/NewProjectPage.tsx
@@ -1,17 +1,14 @@
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import React, {FunctionComponent, useContext, useState} from 'react'
 import SmallFormLayout from '../components/SmallFormLayout'
-import {TextField} from '@mui/material'
 import {Redirect} from 'react-router-dom'
 import {projectUrl} from '../service/urls'
 import {userContext} from '../components/AuthCheck'
 import {UserContext} from '../service/UserContext'
 import LoadingProgress from '../components/LoadingProgress'
 import datatoggle from '@datatoggle/datatoggle-sdk'
+import {TextField} from '@mui/material'
 
 
 
@@ -23,9 +20,16 @@ type ProjectCreationState = {
 const NewProjectPage: FunctionComponent<{ }> = (props) => {
   const ctx: UserContext = useContext(userContext)
   const [name, setName] = useState<string>('')
+  const [nameErrorMessage, setNameErrorMessage] = useState<string | null>(null)
   const [projectCreationState, setProjectCreationState] = useState<ProjectCreationState>({uri: null, creating: false})
 
   async function onCreateProject() {
+    setNameErrorMessage(null)
+    if (!name){
+      setNameErrorMessage('Project name should not be empty')
+      return
+    }
+
     setProjectCreationState({uri: null, creating: true})
     const projectUri: string = await ctx.api.createProject(name)
     datatoggle.track("create_project", {
@@ -45,17 +49,29 @@ const NewProjectPage: FunctionComponent<{ }> = (props) => {
 
   return (
     <SmallFormLayout>
-      <Card sx={{width: '256px'}}>
-        <CardContent sx={{padding: '24px'}}>
-          <Typography variant="h6" component="h2" sx={{paddingBottom: '24px'}}>
-            Choose a name for your project
-          </Typography>
-          <TextField id="standard-basic" label="Project name"  sx={{width: '100%'}} onChange={e => setName(e.target.value)}/>
-        </CardContent>
-        <CardActions sx={{padding: '8px 24px 24px 24px'}}>
-          <Button fullWidth variant="contained" color="primary" disabled={name === ''} onClick={() => onCreateProject()}>Create project</Button>
-        </CardActions>
-      </Card>
+      <Typography variant="h4" component="h2" sx={{paddingBottom: '48px'}}>
+        Choose a name for your project
+      </Typography>
+      <TextField
+        sx={{paddingBottom: '24px'}}
+        variant={'outlined'}
+        fullWidth
+        id='Project Name'
+        label='Project Name'
+        value={name}
+        error={nameErrorMessage != null}
+        helperText={nameErrorMessage}
+        onChange={e => setName(e.target.value)}
+      />
+      <Button
+        fullWidth
+        variant="contained"
+        color="primary"
+        disabled={false}
+        onClick={() => onCreateProject()}
+        size='large'>
+        Create Project
+      </Button>
     </SmallFormLayout>
   );
 }
