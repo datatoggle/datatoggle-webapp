a503d4f0870db39366e9bb4af19327cec83e159a
Nicolas <nicolas.lecrique+git@globers.co>
2022-04-29 12:41:11 +0200

    improve destination panel design

--- src/pages/workspace/destinationpanel/DestinationPanel.tsx
+++ src/pages/workspace/destinationpanel/DestinationPanel.tsx
@@ -1,16 +1,16 @@
 import React, {FunctionComponent, useContext, useEffect, useState} from 'react'
-import Typography from '@mui/material/Typography'
-import {Box, Card, Divider, FormControlLabel, Switch} from '@mui/material'
+import {Box, Card, FormControlLabel, Switch} from '@mui/material'
 import DestinationParamComp from './DestinationParamComp'
 import Button from '@mui/material/Button'
-import CardActions from '@mui/material/CardActions'
-import CardContent from '@mui/material/CardContent'
 import {DestinationConfig, DestinationParam, DestinationParamDef} from '../../../service/restapi/data'
 import {UserContext} from '../../../service/UserContext'
 import {userContext} from '../../../components/AuthCheck'
 import {MyDestination} from '../WorkspacePage'
 import datatoggle from '@datatoggle/datatoggle-sdk'
 import {useParams} from 'react-router-dom'
+import {PanelSection} from '../PanelSection'
+import {backgroundTransparent, messageBackgroundColor} from '../../../DesignConstants'
+import CardContent from '@mui/material/CardContent'
 
 interface OwnProps {
   workspaceUri: string
@@ -37,47 +37,42 @@ const DestinationPanel: FunctionComponent<Props> = (props) => {
   }, [myDestination.configWithInfo.config])
 
   return (
-    <Box sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    }}>
-    <Box sx={{
-      width: '512px',
-      display: 'flex',
-      paddingTop: '64px',
-    }}>
-    </Box>
-      <Card sx={{
-        width: '512px',
-        borderRadius: '16px',
-        padding: '24px'
-      }}>
-        <CardContent sx={{padding: '0'}}>
-          <Box sx={{display: 'flex'}}>
-            <Typography variant="h5" component="h2">
-              {myDestination.definition.name}
-            </Typography>
-            <Box flexGrow={1}/>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={modifiedConfig.isEnabled}
-                onChange={(event, checked) => setModifiedConfig(prevState => {
-                  return {...prevState, isEnabled: checked}
-                })}
-                name="checkedB"
-                color="primary"
-              />
-            }
-            style={ {marginRight: 0}}
-            label="Enabled"
-          />
-          </Box>
-          <Divider sx={{
-            marginTop: '24px',
-            marginBottom: '24px'
-          }}/>
+    <>
+      <PanelSection>
+        <Card sx={{backgroundColor:backgroundTransparent, borderRadius:'8px', padding: '24px'}}>
+      <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'}>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={modifiedConfig.isEnabled}
+              onChange={(event, checked) => setModifiedConfig(prevState => {
+                return {...prevState, isEnabled: checked}
+              })}
+              name="checkedB"
+              color="primary"
+            />
+          }
+          style={ {marginRight: 0, alignSelf: 'flex-end'}}
+          label="Enabled"
+        />
+      <Button
+        variant="contained"
+        color="primary"
+        disabled={myDestination.configWithInfo.config === modifiedConfig}
+        onClick={async () => {
+          const reply = await ctx.api.postDestinationConfig(props.workspaceUri, modifiedConfig)
+          if (reply.saved){
+            props.onDestinationModified(myDestination.definition.uri)
+            datatoggle.track("save_destination_config", {
+              workspace_uri: props.workspaceUri,
+              destination_uri: myDestination.definition.uri
+            })
+          }
+        }}>{myDestination.configWithInfo.config === modifiedConfig ? "Setting saved" : "Save settings"}</Button>
+      </Box>
+        </Card>
+      </PanelSection>
+        <PanelSection>
         {
           paramDefs.map((paramDef: DestinationParamDef) => (
             <DestinationParamComp
@@ -96,28 +91,8 @@ const DestinationPanel: FunctionComponent<Props> = (props) => {
               key={paramDef.uri}/>
           ))
         }
-        </CardContent>
-        <CardActions sx={{
-          padding: '0',
-          display: 'flex',
-          flexDirection: 'row-reverse'}}>
-        <Button
-          variant="contained"
-          color="primary"
-          disabled={myDestination.configWithInfo.config === modifiedConfig}
-          onClick={async () => {
-          const reply = await ctx.api.postDestinationConfig(props.workspaceUri, modifiedConfig)
-          if (reply.saved){
-            props.onDestinationModified(myDestination.definition.uri)
-            datatoggle.track("save_destination_config", {
-              workspace_uri: props.workspaceUri,
-              destination_uri: myDestination.definition.uri
-            })
-          }
-        }}>{myDestination.configWithInfo.config === modifiedConfig ? "Setting saved" : "Save settings"}</Button>
-        </CardActions>
-      </Card>
-    </Box>
+    </PanelSection>
+  </>
   ) // SAVE BUTTON UPDATE PARAMS
 };
 
