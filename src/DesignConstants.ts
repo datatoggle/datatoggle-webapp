166e6dc29933ae5c3ba899d4875a3f08da0441bd
Nicolas <nicolas.lecrique+git@globers.co>
2022-05-05 17:58:18 +0200

    Improve design for dict param field

--- src/DesignConstants.ts
+++ src/DesignConstants.ts
@@ -4,7 +4,11 @@ export const secondaryColor = 'rgb(137, 197, 18)'
 export const messageBackgroundColor = 'rgba(137, 197, 18, 0.7)'
 
 export const standardBorderRadius = '8px'
-
-
-
 export const backgroundTransparent = 'rgba(78, 18, 197, 0.03)'
+
+
+export const textBoxBorderColor = "rgba(0, 0, 0, 0.23)" // same as mui
+export const textBoxLeftPadding = '14px' // same as mui
+export const textBoxVerticalPadding = '16.5px' // same as mui
+export const textBoxBorderRadius = '4px' // same as mui
+export const textBoxColor = "rgba(0, 0, 0, 0.60)"
--- src/pages/workspace/destinationpanel/DestinationParamDict.tsx
+++ src/pages/workspace/destinationpanel/DestinationParamDict.tsx
@@ -1,8 +1,24 @@
 import React, { FunctionComponent } from 'react';
 import {AtomicType, DestinationParamDef} from '../../../service/restapi/data'
 import Typography from '@mui/material/Typography'
-import DestinationParamDictEntry from './DestinationParamDictEntry'
-import Button from '@mui/material/Button'
+import {
+  Box,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField
+} from '@mui/material'
+import {Add, Delete} from '@mui/icons-material'
+import {
+  textBoxBorderColor,
+  textBoxBorderRadius,
+  textBoxLeftPadding, textBoxColor,
+  textBoxVerticalPadding
+} from '../../../DesignConstants'
 
 interface OwnProps {
   paramDef: DestinationParamDef
@@ -13,45 +29,83 @@ interface OwnProps {
 type Props = OwnProps;
 
 const DestinationParamDict: FunctionComponent<Props> = (props) => {
+
+
+  function strToValue(str: string): AtomicType {
+    // return value if number of boolean, else return string
+    try {
+      return JSON.parse(str)
+    } catch (e) {
+      return str
+    }
+  }
+
+
   return (<>
-      <Typography variant="subtitle1">
-      {props.paramDef.name}
-    </Typography>
-      {
-        props.value.map( ([dictKey, dictValue]: [string,AtomicType], idx: number) => {
-          return <DestinationParamDictEntry
-            key={idx}
-            isNew={false}
-            entryKey={dictKey}
-            value={dictValue}
-            onKeyChanged={(newKey: string) => {
-              props.value[idx] = [newKey, dictValue]
-              props.onValueChanged(props.value)
-            }}
-            onValueChanged={(newValue: AtomicType) => {
-              props.value[idx] = [dictKey, newValue]
-              props.onValueChanged(props.value)
-            }}
-            onRemoved={() => {
-              props.value.splice(idx, 1)
-              props.onValueChanged(props.value)
-            }
-            }
-          />
-        })
-      }
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={ () => {
-          let modifiedDict = props.value as [String,AtomicType][]
-          modifiedDict.push(["", ""])
-          props.onValueChanged(props.value)
-        }
-        }
-      >
-        Add entry
-      </Button>
+      <Box sx={{border: 1, borderRadius: textBoxBorderRadius, borderColor:textBoxBorderColor}}>
+        <Box paddingLeft={textBoxLeftPadding} paddingTop={textBoxVerticalPadding} paddingBottom={textBoxVerticalPadding}>
+        <Typography variant="subtitle1" color={textBoxColor}>
+          {props.paramDef.name}
+        </Typography>
+        </Box>
+        <Box padding={'24px'} paddingTop={'8px'}>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ width: 256, borderBottom:"none" }} >Key</TableCell>
+                <TableCell style={{ borderBottom:"none" }}>Value</TableCell>
+                <TableCell style={{ width: 0, borderBottom:"none" }} align="right"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                props.value.map( ([dictKey, dictValue]: [string,AtomicType], idx: number) =>
+                  <TableRow key={idx}>
+                <TableCell  style={{ borderBottom:"none", width: 256 }} >
+                  <TextField value={dictKey} onChange={(event) => {
+                    props.value[idx] = [event.target.value, dictValue]
+                    props.onValueChanged(props.value)
+                  }}/>
+                </TableCell>
+                <TableCell style={{ borderBottom:"none"}}>
+                  <TextField fullWidth value={dictValue}
+                             onChange={(event) => {
+                               props.value[idx] = [dictKey, strToValue(event.target.value)]
+                               props.onValueChanged(props.value)
+                             }}/>
+                </TableCell>
+                <TableCell  style={{ borderBottom:"none", width: 0 }} align="right">
+                  <IconButton color="primary" aria-label="upload picture" component="span" onClick={() => {
+                    props.value.splice(idx, 1)
+                    props.onValueChanged(props.value)
+                  }
+                  }>
+                    <Delete />
+                  </IconButton>
+                </TableCell>
+                </TableRow>)
+              }
+              <TableRow >
+                <TableCell align="left" style={{ borderBottom:"none"}}>
+                  <IconButton color="primary" aria-label="upload picture" component="span"
+                              onClick={ () => {
+                                let modifiedDict = props.value as [String,AtomicType][]
+                                modifiedDict.push(["", ""])
+                                props.onValueChanged(props.value)
+                              }
+                              }>
+                    <Add />
+                  </IconButton>
+                </TableCell>
+                <TableCell style={{ borderBottom:"none"}}></TableCell>
+                <TableCell style={{ borderBottom:"none"}}></TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+        </Box>
+      </Box>
     </>
   );
 };
