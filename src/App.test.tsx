ad4ff18f737af3577fc09d84a3c0c1d42067029a
Nicolas <nicolas.lecrique+git@globers.co>
2022-05-18 14:48:51 +0200

    fix default jest app test (replace default create-react-app)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders learn react link', async () => {
+  render(<App/>);
+  // use find By because we need to wait for firebase.auth async check
+  const linkElement = await screen.findByText("Log Into My Account");
   expect(linkElement).toBeInTheDocument();
 });
